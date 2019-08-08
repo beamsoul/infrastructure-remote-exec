76ab826b3b1d506e5cae51ca8493aea774031bc6
beamsoul <nurzhanasamidinova@gmail.com>
2019-08-07 17:32:08 -0700

    pushing my key

--- key.tf
+++ key.tf
@@ -1,5 +1,5 @@
-resource "aws_key_pair" "developer_key" {
-    key_name = "developer_key"
+resource "aws_key_pair" "winkey-terraf" {
+    key_name = "winkey-terraf"
     public_key = "${file("~/.ssh/id_rsa.pub")}"
   
 }
